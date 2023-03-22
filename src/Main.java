c30cc509c19d1bbf4a4cb8075c8c0945b0206293
Epicskylegend <adeba003@gmail.com>
2023-03-22 17:59:11 -0500

    Added feature to read dictionary text document

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
-import java.util.Random;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main (String args[]) {
+    public static void main (String args[]) throws IOException {
         String [] validCharacters = {"a","A","b","B","c","C","d","D","e","E","f","F","g","G",
                 "h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q",
-                "r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","0","1","2","3","4","5","6","7","8","9","!","@",
-                "#","$","%","^","&","*","-","_","+",".","~","=",};
+                "r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","0","1",
+                "2","3","4","5","6","7","8","9","!","@", "#","$","%","^","&","*","-","_","+",".",
+                "~","=",};
         Scanner password = new Scanner(System.in);
         Random random = new Random();
-        //System.out.println(validCharacters.length);
+        File file = new File("C:\\Users\\adeba\\OneDrive\\Minnehack 2023\\PasswordGenerator\\words.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String word = br.readLine();
+        System.out.println(word);
+        //String word;
+       // while((word = br.readLine()) != null) {
+            //System.out.println(word);
+        //}
+
+            //System.out.println(validCharacters.length);
+
 
 
     System.out.println("Welcome to the password generator. What length would you like your password to be?");
@@ -27,10 +37,11 @@ public class Main {
         randomCharacter = validCharacters[randomIndex];
         newList.add(randomCharacter);
     }
-    String newPassword = String.join("", newList);
-    System.out.println(newPassword);
-    //
 
+    String newPassword = String.join("", newList);
+
+    System.out.println(newPassword);
+   // System.out.println(newPassword.hashCode());
 
 
 }
